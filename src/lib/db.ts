782bdb0685ebee69f046327f0b6034bc84b3f6f4
keelangibb <keelangibb@gmail.com>
2023-08-28 09:46:48 -0500

    added env validation, setup db with planetscale

---
+++ src/lib/db.ts
@@ -0,0 +1,18 @@
+import { env } from "@/env.mjs";
+import { PrismaClient } from "@prisma/client";
+
+// prevents new PrismaClient() from running on every Hot Module Reload
+// https://www.prisma.io/docs/guides/performance-and-optimization/connection-management#prismaclient-in-long-running-applications
+
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
+
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    log:
+      env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
+  });
+
+if (env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
