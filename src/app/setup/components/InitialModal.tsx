e1d67f37234e9df8e83af6d85268c6ff6b6f82f0
keelangibb <keelangibb@gmail.com>
2023-09-01 23:34:22 -0500

    fixed center modal on setup route

--- src/app/setup/components/InitialModal.tsx
+++ src/app/setup/components/InitialModal.tsx
@@ -56,73 +56,76 @@ export default function InitialModal() {
     }
     console.log(values);
   };
+
   return (
-    <Dialog open>
-      <DialogContent className="overflow-hidden bg-white p-0 text-black">
-        <DialogHeader className="px-6 pt-8">
-          <DialogTitle className="text-center text-2xl font-bold">
-            Customize your server
-          </DialogTitle>
-          <DialogDescription className="text-center text-zinc-500">
-            Give your server a personality with a name and an image. You can
-            always change it later.
-          </DialogDescription>
-        </DialogHeader>
-        <Form {...form}>
-          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-            <div className="space-y-8 px-6">
-              <div className="flex items-center justify-center text-center">
+    <>
+      <Dialog open>
+        <DialogContent className="overflow-hidden bg-white p-0 text-black">
+          <DialogHeader className="px-6 pt-8">
+            <DialogTitle className="text-center text-2xl font-bold">
+              Customize your server
+            </DialogTitle>
+            <DialogDescription className="text-center text-zinc-500">
+              Give your server a personality with a name and an image. You can
+              always change it later.
+            </DialogDescription>
+          </DialogHeader>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+              <div className="space-y-8 px-6">
+                <div className="flex items-center justify-center text-center">
+                  <FormField
+                    control={form.control}
+                    name="imageUrl"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormControl>
+                          <FileUpload
+                            endpoint={"serverImage"}
+                            value={field.value}
+                            onChange={field.onChange}
+                          />
+                        </FormControl>
+                      </FormItem>
+                    )}
+                  />
+                </div>
                 <FormField
                   control={form.control}
-                  name="imageUrl"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormControl>
-                        <FileUpload
-                          endpoint={"serverImage"}
-                          value={field.value}
-                          onChange={field.onChange}
-                        />
-                      </FormControl>
-                    </FormItem>
-                  )}
+                  name="name"
+                  render={({ field }) => {
+                    return (
+                      <FormItem>
+                        <FormLabel className="tex-xs fontt-bold uppercase text-zinc-500 dark:text-secondary/70">
+                          Server Name
+                        </FormLabel>
+                        <FormControl>
+                          <Input
+                            disabled={isLoading}
+                            className="focus-visable:ring-0 forcus:visible:rign-ffset-0 border-0 bg-zinc-300/50 text-black"
+                            placeholder="Enter a server name"
+                            {...field}
+                          />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    );
+                  }}
                 />
               </div>
-              <FormField
-                control={form.control}
-                name="name"
-                render={({ field }) => {
-                  return (
-                    <FormItem>
-                      <FormLabel className="tex-xs fontt-bold uppercase text-zinc-500 dark:text-secondary/70">
-                        Server Name
-                      </FormLabel>
-                      <FormControl>
-                        <Input
-                          disabled={isLoading}
-                          className="focus-visable:ring-0 forcus:visible:rign-ffset-0 border-0 bg-zinc-300/50 text-black"
-                          placeholder="Enter a server name"
-                          {...field}
-                        />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  );
-                }}
-              />
-            </div>
-            <DialogFooter className="bg-gray-100 px-6 py-4">
-              <Button variant={"primary"} disabled={isLoading}>
-                Create
-              </Button>
-              <Button onClick={callApi} type="button">
-                Click me
-              </Button>
-            </DialogFooter>
-          </form>
-        </Form>
-      </DialogContent>
-    </Dialog>
+              <DialogFooter className="bg-gray-100 px-6 py-4">
+                <Button variant={"primary"} disabled={isLoading}>
+                  Create
+                </Button>
+                <Button onClick={callApi} type="button">
+                  Click me
+                </Button>
+              </DialogFooter>
+            </form>
+          </Form>
+        </DialogContent>
+      </Dialog>
+    </>
   );
 }
 
